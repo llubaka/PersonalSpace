0a6da38934646fb5338a70f6edefffb2de11d377
llubaka <llubaka1995@gmail.com>
2022-06-11 22:29:48 +0300

    Updated task component with shadow and other effects.

--- src/components/Task/Task.tsx
+++ src/components/Task/Task.tsx
@@ -1,5 +1,4 @@
-import moment from "moment";
-import React from "react";
+import React, { useCallback } from "react";
 import styled from "styled-components";
 import { convetDateTo_Day_Month_Year } from "../../helpers";
 
@@ -9,14 +8,40 @@ interface TaskProps {
   dateStart: Date;
   dateEnd: Date;
   dateCreated: Date;
+  isFinished?: boolean;
+  customShadowColor?: string;
 }
 
-export const Task: React.FC<TaskProps> = ({ title, content, dateStart, dateEnd, dateCreated }) => {
+enum TaskStatus {
+  NOT_STARTED,
+  IN_PROCESS,
+  TIMED_OVER,
+  FINISHED_SUCCESSFUL,
+}
+
+export const Task: React.FC<TaskProps> = ({
+  title,
+  content,
+  dateStart,
+  dateEnd,
+  dateCreated,
+  isFinished,
+  customShadowColor,
+}) => {
+  const getTaskStatus = useCallback((): TaskStatus => {
+    if (isFinished) return TaskStatus.FINISHED_SUCCESSFUL;
+
+    const dateNow = new Date();
+    if (dateNow > dateStart && dateNow < dateEnd) return TaskStatus.IN_PROCESS;
+    else if (dateStart > dateNow) return TaskStatus.NOT_STARTED;
+    else return TaskStatus.TIMED_OVER;
+  }, [dateEnd, dateStart, isFinished]);
+
   return (
-    <TaskStyled>
+    <TaskStyled taskStatus={getTaskStatus()} customShadowColor={customShadowColor}>
       <div>
-        <div>{convetDateTo_Day_Month_Year(dateStart)}</div>
-        <div>{convetDateTo_Day_Month_Year(dateEnd)}</div>
+        <div>started: {convetDateTo_Day_Month_Year(dateStart)}</div>
+        <div>ended: {convetDateTo_Day_Month_Year(dateEnd)}</div>
       </div>
       <div>{title}</div>
       <div>{content}</div>
@@ -24,13 +49,25 @@ export const Task: React.FC<TaskProps> = ({ title, content, dateStart, dateEnd, 
   );
 };
 
-const TaskStyled = styled.div<{}>((props) => {
-  return {
-    boxShadow: "-2px 2px 5px 0px rgba(9,104,145,1)",
-    margin: "10px",
-    padding: "15px 30px",
-    borderRadius: "5px",
-    background: "white",
-    "&:hover": {},
-  };
-});
+const TaskStyled = styled.div<{ taskStatus: TaskStatus; customShadowColor?: string }>(
+  ({ taskStatus, customShadowColor }) => {
+    const shadowColor = {
+      [TaskStatus.NOT_STARTED]: "rgba(135,148,136,1)",
+      [TaskStatus.IN_PROCESS]: "rgba(9,104,145,1)",
+      [TaskStatus.TIMED_OVER]: "rgba(247,5,5,1)",
+      [TaskStatus.FINISHED_SUCCESSFUL]: "rgba(6,181,23,1)",
+    };
+
+    return {
+      boxShadow: `-1px 1px 6px 2px ${customShadowColor || shadowColor[taskStatus]}`,
+      margin: "10px",
+      padding: "15px 30px",
+      borderRadius: "5px",
+      background: "white",
+      "&:hover": {
+        boxShadow: `-2px 2px 7px 3px ${customShadowColor || shadowColor[taskStatus]}`,
+        transform: "scale(99%)",
+      },
+    };
+  }
+);
--- src/pages/home/home.tsx
+++ src/pages/home/home.tsx
@@ -9,8 +9,39 @@ const Home = () => {
         title="The title"
         content="This is content of the task."
         dateCreated={new Date()}
-        dateEnd={new Date()}
-        dateStart={new Date()}
+        dateEnd={new Date(2024, 5, 21)}
+        dateStart={new Date(2020, 5, 21)}
+      />
+      <Task
+        title="The title"
+        content="This is content of the task."
+        dateCreated={new Date()}
+        dateEnd={new Date(2020, 5, 21)}
+        dateStart={new Date(2019, 5, 21)}
+      />
+      <Task
+        title="The title"
+        content="This is content of the task."
+        dateCreated={new Date()}
+        dateEnd={new Date(2026, 5, 21)}
+        dateStart={new Date(2024, 5, 21)}
+      />
+      <Task
+        title="The title"
+        content="This is content of the task."
+        dateCreated={new Date()}
+        dateEnd={new Date(2026, 5, 21)}
+        dateStart={new Date(2024, 5, 21)}
+        isFinished
+      />
+      <Task
+        title="The title"
+        content="This is content of the task."
+        dateCreated={new Date()}
+        dateEnd={new Date(2026, 5, 21)}
+        dateStart={new Date(2024, 5, 21)}
+        isFinished
+        customShadowColor="rgba(12,55,233)"
       />
     </div>
   );
