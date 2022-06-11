6cc4dc8d69bdab0d0ce0723d7b8d3c6b1246ec8e
llubaka <llubaka1995@gmail.com>
2022-06-11 13:38:15 +0300

    Update folder structure and add browser router.

---
+++ src/App/App.tsx
@@ -0,0 +1,15 @@
+import React from "react";
+import { Routes, Route } from "react-router-dom";
+import Home from "../pages/home";
+import NotFound from "../pages/notFound";
+
+function App() {
+  return (
+    <Routes>
+      <Route path="/" element={<Home />} />
+      <Route path="*" element={<NotFound />} />
+    </Routes>
+  );
+}
+
+export default App;
